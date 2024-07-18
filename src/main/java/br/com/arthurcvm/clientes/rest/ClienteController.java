c48f685a537c5322d7845ae3058f76e370feb1c9
Arthur Cezar <arthurcvm@hotmail.com>
2024-07-18 19:06:45 -0300

    ajuste de cors

--- src/main/java/br/com/arthurcvm/clientes/rest/ClienteController.java
+++ src/main/java/br/com/arthurcvm/clientes/rest/ClienteController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 @RequiredArgsConstructor
 public class ClienteController {
 
